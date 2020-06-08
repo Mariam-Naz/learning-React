a7b3f5b1f7eb3e1f06e9e4370f9daadf3f2b1a79
Mariam_Naz <54590203+Mariam-Naz@users.noreply.github.com>
2020-06-08 23:30:05 +0500

    js inner html inner js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const age = 20;
+
 ReactDom.render(
 <div>
-  <h1> my favourite books </h1>
-  <ul>
-    <li> Quran </li>
-    <li> zavia </li>
-    <li> namal </li>
-  </ul>
-  </div>, document.getElementById('root'));
+  <h1> Assalam o Alaikum Mariam </h1>
+  <p>
+    my age is {age}
+  </p>
+</div>, document.getElementById('root'));
