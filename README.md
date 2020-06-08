4efe43346aa84d7d6a9d885869c5d474907d24fe
Mariam_Naz <54590203+Mariam-Naz@users.noreply.github.com>
2020-06-08 23:18:46 +0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# learning-React
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+<div>
+  <h1> my favourite books </h1>
+  <ul>
+    <li> Quran </li>
+    <li> zavia </li>
+    <li> namal </li>
+  </ul>
+  </div>, document.getElementById('root'));
