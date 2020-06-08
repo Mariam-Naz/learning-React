d952c762f61ab43b00a35a4443be74e29d7d5a60
Mariam_Naz <54590203+Mariam-Naz@users.noreply.github.com>
2020-06-08 23:43:13 +0500

    working with JSX and Js expressions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const age = 20;
+const fName = 'Mariam';
+const lName = 'Naz';
+const age = Math.floor(Math.random()*10);
 
 ReactDom.render(
 <div>
-  <h1> Assalam o Alaikum Mariam </h1>
+  <h1> Assalam o Alaikum {fName +' '+ lName} </h1>
   <p>
-    my age is {age}
+    Your lucky number is {age}
   </p>
 </div>, document.getElementById('root'));
