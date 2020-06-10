f46c2b0b902e80b9c104a27b71b6fc0f335e85a7
unknown <mariamnaz212@gmail.com>
2020-06-10 12:31:01 +0500

    inline styling for react elements

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDom from "react-dom";
 
 const image = 'https://picsum.photos/200'
 
+const imageCustomize = {
+    border: '2px solid black', // values in string
+    boxShadow: ' 3px 3px 5px 6px #ccc'
+}
+
 ReactDom.render(
 <div>
-  <img alt='nature' src={image+'?grayscale'}/>
+  <img style={imageCustomize} // style={{object}} object contain css property
+  alt='nature' src={image+'?grayscale'}/>
   </div>, document.getElementById('root'));
