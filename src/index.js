b3a84a8a07b16941e41dd7bc12fb048d615075ce
unknown <mariamnaz212@gmail.com>
2020-06-11 14:01:01 +0500

    react styling practice updated

--- src/index.js
+++ src/index.js
@@ -5,23 +5,22 @@ import ReactDom from "react-dom";
 let time = new Date(2020,11,20);
 let timeSet = time.getHours();
 
+var colour = {
+  color : ''
+    }
+
 if(timeSet >0 && timeSet<=12){
   var greetMsg = 'Good Morning';
-  var colour = {
-color : 'red'
-  }
+  colour.color = 'red';
+  
 }
 else if(timeSet >12 && timeSet<=18){
    greetMsg = 'Good Afternoon';
-    colour = {
-    color : 'green'
-      }
+   colour.color = 'green';
 }
 else{
   greetMsg = 'Good Night'
-   colour = {
-    color : 'blue'
-      }
+  colour.color = 'blue';
 }
 ReactDom.render(
 <div>
