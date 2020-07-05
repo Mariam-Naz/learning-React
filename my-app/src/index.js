287b9a67b6a7269859822ab2bd2404d1a4e3f0f3
unknown <mariamnaz212@gmail.com>
2020-07-05 22:36:45 +0500

    Desructive array and objects

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,31 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App"
+import cars from "./practice";
 
-ReactDOM.render(<App />,document.getElementById("root")
+const [honda , tesla] = cars;
+
+const {coloursByPopularity : [teslaTopColour],speedStats :{topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour],speedStats :{ topSpeed: hondaTopSpeed  }} = honda;
+
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
 );
