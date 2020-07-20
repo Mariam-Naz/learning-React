480a2022eb6dcdf0cd315bdf53e1b12cfb1bf919
unknown <mariamnaz212@gmail.com>
2020-07-20 21:27:57 +0500

    React Complex state

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,23 +1,36 @@
-import React, {useState}from "react";
+import React , {useState} from "react";
 
 function App() {
-const [name, setName] = useState("");
-const [showname , setShowname] = useState("");
-function handleChange(event){
-  setName(event.target.value);
-}
+  const [fullName , setFullName] = useState({
+    fName : "",
+    lName : ""
+  });
 
-function displayName(){
-  setShowname(name);
-}
+  function handleChange(event){
+    const{value , name} = event.target;
 
+    setFullName(preValue =>{
+      if(name === "fName"){
+        return{
+          fName: value,
+          lname: preValue.lName
+      }
+      }else if(name === "lName"){
+        return{
+          fName: preValue.fName,
+          lName: value
+      }
+      }
+    });
+  }
   return (
     <div className="container">
-      <h1>Hello {showname}</h1>
-      <input onChange = {handleChange} 
-      type="text" 
-      placeholder="What's your name?" />
-      <button onClick = {displayName}>Submit</button>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <form>
+        <input onChange = {handleChange}  name="fName" placeholder="First Name" value={fullName.fName} />
+        <input onChange = {handleChange}  name="lName" placeholder="Last Name" value={fullName.lName}/>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
