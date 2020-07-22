8c4d8088828e2f34767e0f2db3f98723c531aeb6
unknown <mariamnaz212@gmail.com>
2020-07-23 02:10:27 +0500

    Es6 spread operator

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -6,30 +6,16 @@ function App() {
     lName: "",
     email: ""
   });
-  function showContact(event){
-    const{value, name} = event.target;
-    setContact(preValue => {
-      if(name === 'fName'){
-        return{
-          fName: value,
-          lName: preValue.lName,
-          email: preValue.email
-        }
-      } else if(name === 'lName'){
-        return{
-          fName: preValue.fName,
-          lName: value,
-          email: preValue.email
-        }
-      } else if(name === 'email'){
-        return{
-          fName: preValue.fName,
-          lName: preValue.lName,
-          email: value
-        }
-      } 
-    })
+ 
+  function handleChange(event) {
+    const { name, value } = event.target;
 
+    setContact(prevValue => {
+        return {
+          ...prevValue,
+          [name] : value
+        };
+      }); 
   }
   return (
     <div className="container">
@@ -38,9 +24,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input onChange={showContact} name="fName" placeholder="First Name" />
-        <input onChange={showContact} name="lName" placeholder="Last Name" />
-        <input onChange={showContact} name="email" placeholder="Email" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
         <button>Submit</button>
       </form>
     </div>
